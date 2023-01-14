d2c7fe261ad0b1d9491d59346f8bf9808f3ea999
Harout Terterian <hyecartoon@gmail.com>
2023-01-14 15:07:32 -0800

    Dynamic rows working with info from TMDB api

---
+++ src/components/Row/Row.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "../../api/axios";
+import "./Row.css";
+
+function Row({ title, fetchURL, isLargeRow = false }) {
+  const [movie, setMovie] = useState([]);
+
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovie(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movie.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt=""
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
